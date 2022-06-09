de1222eb8e73adaff65c171c1ccb621f33f250ab
Gustavo Correa <gustavocorrea66@gmail.com>
2022-06-08 22:37:54 -0300

    <FEATURE>: Adicionado metodo criacao user firebase

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_firebase_project/model/user.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
@@ -20,30 +22,64 @@ class UserForm extends HomeScreen {
             IconButton(
               icon: const Icon(Icons.save),
               onPressed: () {
-                Navigator.of(context).pop();
+                final isValid = _form.currentState?.validate();
+
+                if (isValid == true) {
+                  _form.currentState?.save();
+                  final user = User(
+                      name: _formData['name'],
+                      age: _formData['age'],
+                      gender: _formData['gender']);
+                  Navigator.of(context).pop();
+                }
               },
             ),
           ],
         ),
         body: Padding(
-          padding: EdgeInsets.all(15),
+          padding: const EdgeInsets.all(15),
           child: Form(
+            key: _form,
             child: Column(
               children: <Widget>[
                 TextFormField(
                   decoration: const InputDecoration(
                     labelText: 'Nome',
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nome invalido';
+                    }
+                    if (value.trim().length < 4) {
+                      return 'Nome muito pequeno. Minimo 4 letras';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _formData['genero'],
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Idade'),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length > 3) {
+                      return 'Idade invalida';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _formData['age'],
                 ),
                 TextFormField(
                   decoration: const InputDecoration(
-                    labelText: 'Idade',
+                    labelText: 'Orientacao Sexual',
                   ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Genero',
-                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().length > 2) {
+                      return 'Minimo 2 letras';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _formData['gender'],
                 )
               ],
             ),
@@ -52,4 +88,15 @@ class UserForm extends HomeScreen {
       ),
     );
   }
+}
+
+Future createUser(
+    {required String name, required String age, required String gender}) async {
+  final docUser = FirebaseFirestore.instance.collection('users').doc();
+
+  final user = User(id: docUser.id, name: name, age: age, gender: gender);
+
+  final json = user.toJson();
+
+  await docUser.set(json);
 }
